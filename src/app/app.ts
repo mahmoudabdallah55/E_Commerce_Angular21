c66b06968a79ed6f6930b9a9bf3c6e61137cd331
Mahmoud Abdallah <mahmoudabdallah084@gmail.com>
2026-03-05 19:36:03 +0200

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule, MatProgressSpinnerModule, MatSlideToggleModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
